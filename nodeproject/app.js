dcba3ab03696050cd021d5e1de0cc83be74e4d19
vedran d <vdunkic@gmail.com>
2017-01-10 08:41:27 +0000

    Nakon prvog koraka. Radi node, mongo, ejs i gulp

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,37 @@
-var pozdrav = 'vozdra';
-console.log(pozdrav);
+var express = require("express");
+var app = express();
+var port = process.env.PORT;
+var eventRouter = require('./src/routes/eventRoutes');
+var dbRouter = require("./src/routes/dbRoutes")
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+
+
+app.use('/Events', eventRouter);
+app.use('/Db', dbRouter);
+
+app.get('/', function (req, res) {
+    // res.send('Đes ba');
+    res.render('index', {
+        list: ['first val', '2nd val', '3rd val'],
+        nav: [{Link: 'Services', Text:'Services'},
+            {Link: 'Portfolio',Text: 'Portfolio'},
+            {Link: 'About', Text: 'About'},
+            {Link: 'Team', Text:'Team'},
+            {Link: 'Contact', Text:'Contact'},
+            {Link: 'Events', Text:'Events'}
+            ]
+    });
+});
+
+app.listen(port, function (err) {
+    // body...
+    console.log('Server je na portu ' + port);
+    
+})
